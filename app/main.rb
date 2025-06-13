6545fb22158030aad9f1c909845f2298d58b970f
Seb <everydayseb@pm.me>
2025-06-13 06:28:29 +0100

    Add bg and pipes

--- app/main.rb
+++ app/main.rb
@@ -1,85 +1,95 @@
+# Logical canvas width and height
+WIDTH = 720
+HEIGHT = 1280
+
+# pixel art dimensions
+PIXEL_WIDTH = 180
+PIXEL_HEIGHT = 320
+
+# Determine best fit zoom level
+ZOOM_WIDTH = (WIDTH / PIXEL_WIDTH).floor
+ZOOM_HEIGHT = (HEIGHT / PIXEL_HEIGHT).floor
+ZOOM = [ZOOM_WIDTH, ZOOM_HEIGHT].min
+
+# Compute the offset to center the screen
+OFFSET_X = (WIDTH - PIXEL_WIDTH * ZOOM) / 2
+OFFSET_Y = (HEIGHT - PIXEL_HEIGHT * ZOOM) / 2
+
+# Compute the scaled dimensions of the screen
+ZOOMED_WIDTH = PIXEL_WIDTH * ZOOM
+ZOOMED_HEIGHT = PIXEL_HEIGHT * ZOOM
+
+
 def tick args
-  args.state.logo_rect ||= { x: 576,
-                             y: 200,
-                             w: 128,
-                             h: 101 }
 
-  args.outputs.labels  << { x: 640,
-                            y: 600,
-                            text: 'Hello World!',
-                            size_px: 30,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  defaults args
+  render args
+  calc args
+end
 
-  args.outputs.labels  << { x: 640,
-                            y: 510,
-                            text: "Documentation is located under the ./docs directory. 150+ samples are located under the ./samples directory.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+GTK.reset
 
-  args.outputs.labels  << { x: 640,
-                            y: 480,
-                            text: "You can also access these docs online at docs.dragonruby.org.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+def defaults args
+  args.state.pipes ||= []
+end
 
-  args.outputs.labels  << { x: 640,
-                            y: 400,
-                            text: "The code that powers what you're seeing right now is located at ./mygame/app/main.rb.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+def render args
+  # bg color
+  args.outputs.background_color = [20, 24, 46]
 
-  args.outputs.labels  << { x: 640,
-                            y: 380,
-                            text: "(you can change the code while the app is running and see the updates live)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  # define a render target that represents the pixel art canvas dimensions
+  args.outputs[:pixel_canvas].w = PIXEL_WIDTH
+  args.outputs[:pixel_canvas].h = PIXEL_HEIGHT
+  args.outputs[:pixel_canvas].background_color = [20, 24, 46]
 
-  args.outputs.sprites << { x: args.state.logo_rect.x,
-                            y: args.state.logo_rect.y,
-                            w: args.state.logo_rect.w,
-                            h: args.state.logo_rect.h,
-                            path: 'dragonruby.png',
-                            angle: Kernel.tick_count }
+  # draw to render target
+  args.outputs[:pixel_canvas].sprites << {x: 0, y: 0, w: 180, h: 320, path: 'sprites/bg.png'}
 
-  args.outputs.labels  << { x: 640,
-                            y: 180,
-                            text: "(use arrow keys to move the logo around)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  # scale render target and draw to screen
+  args.outputs.sprites << { x: WIDTH / 2, y: HEIGHT / 2, 
+                            w: ZOOMED_WIDTH, h: ZOOMED_HEIGHT,
+                            anchor_x: 0.5, anchor_y: 0.5,
+                            path: :pixel_canvas }
 
-  args.outputs.labels  << { x: 640,
-                            y: 80,
-                            text: 'Join the Discord Server! https://discord.dragonruby.org',
-                            size_px: 30,
-                            anchor_x: 0.5 }
+  args.outputs[:pixel_canvas].sprites << args.state.pipes
+end
 
-  if args.inputs.keyboard.left
-    args.state.logo_rect.x -= 10
-  elsif args.inputs.keyboard.right
-    args.state.logo_rect.x += 10
+def calc args
+  spawn_pipes args
+  move_pipes args
+end
+
+def spawn_pipes args
+  if args.state.pipes_spawned_at.elapsed_time >= 150
+    gap_height = 30
+    bottom_y = 0 - Numeric.rand(40..200) 
+    #top_y = PIXEL_HEIGHT + bottom_y + gap_height
+    top_y = PIXEL_HEIGHT + bottom_y + gap_height
+    top_pipe = pipe(180, top_y, 44, 219, true)
+    bottom_pipe = pipe(180, bottom_y, 44, 219, false)
+
+    args.state.pipes << top_pipe
+    args.state.pipes << bottom_pipe
+    args.state.pipes_spawned_at = Kernel.tick_count
   end
+end
 
-  if args.inputs.keyboard.down
-    args.state.logo_rect.y -= 10
-  elsif args.inputs.keyboard.up
-    args.state.logo_rect.y += 10
+def move_pipes args
+  args.state.pipes.each do |pipe| 
+    pipe.x = pipe.x.lerp(pipe.x - 2, 0.5)
+    #pipe.x -= 1
   end
+  
+  args.state.pipes.reject! {|pipe| pipe.x < 0 - pipe.w}
+end
 
-  if args.state.logo_rect.x > 1280
-    args.state.logo_rect.x = 0
-  elsif args.state.logo_rect.x < 0
-    args.state.logo_rect.x = 1280
-  end
-
-  if args.state.logo_rect.y > 720
-    args.state.logo_rect.y = 0
-  elsif args.state.logo_rect.y < 0
-    args.state.logo_rect.y = 720
-  end
+def pipe x, y, w, h, flipped
+  {
+    x: x,
+    y: y,
+    w: w,
+    h: h,
+    path: "sprites/pipe.png",
+    flip_vertically: flipped,
+  }
 end
